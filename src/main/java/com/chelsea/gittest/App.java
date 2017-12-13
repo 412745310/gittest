cfafea588eb98ca11b5a81013b11119cdf021773
412745310@qq.com <412745310@qq.com>
2017-12-13 10:12:40 +0800

    提交gittest测试

---
+++ src/main/java/com/chelsea/gittest/App.java
@@ -0,0 +1,13 @@
+package com.chelsea.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
