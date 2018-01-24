771ce3653e6f1c6daa9b6ccec5cf9c85fbba75a6
412745310@qq.com <412745310@qq.com>
2018-01-24 09:35:50 +0800

    修改helloworld

--- src/main/java/com/chelsea/gittest/App.java
+++ src/main/java/com/chelsea/gittest/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "1" );
         System.out.println( "2" );
+        System.out.println( "3" );
+        System.out.println( "5" );
     }
 }
